50c1890ef29ff4127af33b73c477ba7a58aae330
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-09 21:16:24 -0300

    Added Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
+
+function Header({ navigation, cartSize }) {
+  return (
+    <Wrapper>
+      <Container>
+        <Logo />
+        <BasketContainer>
+          <Icon name="shopping-basket" color="#FFF" size={24} />
+          <ItemCount>{cartSize || 0}</ItemCount>
+        </BasketContainer>
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from './pages/Main';
 import Cart from './pages/Cart';
+
+import Header from './components/Header';
+
+import colors from './styles/colors';
 
 const Routes = createAppContainer(
   createStackNavigator(
@@ -11,9 +16,11 @@ const Routes = createAppContainer(
       Cart,
     },
     {
-      defaultNavigationOptions: {
-        headerTitleAlign: 'center',
-        headerBackTitleVisible: false,
+      defaultNavigationOptions: navigation => ({
+        header: <Header {...navigation} />,
+      }),
+      cardStyle: {
+        backgroundColor: colors.dark,
       },
     }
   )
