45c9e1d38bff5b0ab9759df0789412178a8c79fc
Anderson Begossi <andersonbegossi@gmail.com>
2020-03-10 14:57:02 -0300

    Stylized Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
-import { Container, Product, ProductImage } from './styles';
+import {
+  Container,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  ProductAmount,
+  ProductAmountText,
+  AddButtonText,
+} from './styles';
 
 export default class Main extends Component {
   state = {
@@ -16,7 +27,6 @@ export default class Main extends Component {
   }
 
   getProducts = async () => {
-    console.tron.log('chegou auqi');
     const response = await api.get('/products');
 
     const data = response.data.map(product => ({
@@ -31,6 +41,15 @@ export default class Main extends Component {
     return (
       <Product key={item.id}>
         <ProductImage source={{ uri: item.image }} />
+        <ProductTitle>{item.title}</ProductTitle>
+        <ProductPrice>{formatPrice(item.price)}</ProductPrice>
+        <AddButton>
+          <ProductAmount>
+            <Icon name="add-shopping-cart" color="#fff" size={20} />
+            <ProductAmountText>{0}</ProductAmountText>
+          </ProductAmount>
+          <AddButtonText>ADICIONAR</AddButtonText>
+        </AddButton>
       </Product>
     );
   };
